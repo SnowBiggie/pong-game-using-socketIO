d56cdcee1dcf05c81c7f04962168ad358be1da4d
Karabelo <snowmotaung@gmail.com>
2023-03-25 22:06:18 +0200

    finished the ball and paddle movement

--- pongGame/server.js
+++ pongGame/server.js
@@ -1,16 +1,41 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST']
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+const PORT = 3000;
+
+
+server.listen(PORT);
+console.log(`Listening on port ${PORT}...`);
+
+let readyPlayerCount = 0;
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount &= 2) {
+      io.emit('startGame', socket.id);
     }
   });
 
-const PORT = 3000;
-server.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`)
-});
+  socket.on('paddleMove', (paddleData) => {
+    socket.broadcast.emit('paddleMove', paddleData);
+  });
 
-io.on('connection', (socket)=>{
-    console.log('A user is connected');
-})
+  socket.on('ballMove', (ballData)=>{
+    socket.broadcast.emit('ballMove', ballData);
+  });
+
+  socket.on('disconnect', (reason)=>{
+
+  })
+});
